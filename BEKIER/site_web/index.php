36f2dae6708b64634a649236f7561cd66bb4de78
Max094Reikeb <49318596+Max094Reikeb@users.noreply.github.com>
2023-04-15 14:27:59 +0200

    Modified style

--- BEKIER/site_web/index.php
+++ BEKIER/site_web/index.php
@@ -8,22 +8,24 @@
 <body>
 <h1>Prévisions météo en France</h1>
 <!-- Ajout de la carte avec les régions -->
-<?php include 'carte.php'; ?>
+<div class="container">
+    <?php include 'carte.php'; ?>
 
-<!-- Formulaire pour sélectionner le département et la ville -->
-<form id="form-meteo" action="meteo.php" method="post">
-    <div id="select-department">
-        <select>
-            <option value="">Sélectionnez un département</option>
-        </select>
-    </div>
-    <div id="select-ville">
-        <select disabled>
-            <option value="">Sélectionnez une ville</option>
-        </select>
-    </div>
-    <input type="submit" value="Voir la météo" disabled>
-</form>
+    <!-- Formulaire pour sélectionner le département et la ville -->
+    <form id="form-meteo" action="meteo.php" method="post">
+        <div id="select-department">
+            <select disabled>
+                <option value="">Sélectionnez un département</option>
+            </select>
+        </div>
+        <div id="select-ville">
+            <select disabled>
+                <option value="">Sélectionnez une ville</option>
+            </select>
+        </div>
+        <input type="submit" value="Voir la météo" disabled>
+    </form>
+</div>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="js/app.js"></script>
