7b769067562ef2b6227236603b10d6033e204aff
GLacaille <60034772+Goutcho@users.noreply.github.com>
2023-04-20 15:33:01 +0200

    Update cli.py

--- BEKIER_LACAILLE/src/cli.py
+++ BEKIER_LACAILLE/src/cli.py
@@ -1,15 +1,14 @@
 import argparse
 import os
 import mimetypes
-from typing import Union
-
+from typing import Union, List
+from xml.etree.ElementTree import Element, SubElement, Comment, tostring
+from xml.dom import minidom
 from tinytag import TinyTag
-
 
 def extract_metadata(file_path: str) -> Union[None, dict]:
     """
     Fonction pour extraire les métadonnées d'un fichier MP3 ou FLAC.
-
     :param file_path: Chemin d'accès au fichier MP3 ou FLAC.
     :return: Un dictionnaire contenant les métadonnées si le fichier est valide, sinon None.
     """
@@ -31,7 +30,7 @@ def extract_metadata(file_path: str) -> Union[None, dict]:
     except Exception as e:
         print(f"Erreur lors de l'extraction des métadonnées: {e}")
         return None
-    
+
 class MusicFileExplorer:
     def __init__(self, root_directory):
         self.root_directory = root_directory
@@ -46,7 +45,7 @@ class MusicFileExplorer:
 
         if directory is None:
             directory = self.root_directory
-.
+
         for entry in os.scandir(directory):
             if entry.is_file() and self.is_music_file(entry.path):
                 self.music_files.append(entry.path)
@@ -77,13 +76,43 @@ class MusicFileExplorer:
             list: La liste des fichiers musicaux trouvés.
         """
         return self.music_files
-    
 
+def create_xspf_playlist(music_files: List[str], output_path: str):
+        """
+    Cette méthode crée une playlist XSPF à partir d'une liste de fichiers musicaux et enregistre la playlist
+    dans un fichier spécifié par output_path.
+
+    Args:
+        music_files (List[str]): Une liste de chemins de fichiers musicaux à inclure dans la playlist.
+        output_path (str): Le chemin d'accès du fichier où la playlist XSPF sera enregistrée.
+
+    """
+    playlist = Element("playlist", version="1", xmlns="http://xspf.org/ns/0/")
+    track_list = SubElement(playlist, "trackList")
+
+    for music_file in music_files:
+        metadata = extract_metadata(music_file)
+        if metadata:
+            track = SubElement(track_list, "track")
+            SubElement(track, "location").text = music_file
+            SubElement(track, "title").text = metadata["title"]
+            SubElement(track, "creator").text = metadata["artist"]
+            SubElement(track, "album").text = metadata["album"]
+            SubElement(track, "duration").text = str(int(metadata["duration"] * 1000))
+            SubElement(track, "trackNum").text = str(metadata["track_number"])
+            SubElement(track, "meta", rel="year").text = metadata["year"]
+            SubElement(track, "meta", rel="genre").text = metadata["genre"]
+
+    pretty_playlist = minidom.parseString(tostring(playlist, "utf-8")).toprettyxml(indent="  ")
+
+    with open(output_path, "w", encoding="utf-8") as output_file:
+        output_file.write(pretty_playlist)
 
 def main():
     parser = argparse.ArgumentParser(description="Explorer et extraire les métadonnées des fichiers musicaux.")
     parser.add_argument('-d', '--directory', type=str, help='Chemin d\'accès au répertoire racine')
     parser.add_argument('-f', '--file', type=str, help='Chemin d\'accès au fichier MP3 ou FLAC')
+    parser.add_argument('-o', '--output', type=str, help='Chemin d\'accès à la playlist XSPF à créer')
     args = parser.parse_args()
 
     if args.directory is None and args.file is None:
@@ -93,6 +122,9 @@ def main():
             music_explorer = MusicFileExplorer(args.directory)
             music_explorer.explore_directory()
             filtered_music_files = music_explorer.get_music_files()
+
+            if args.output is not None:
+                create_xspf_playlist(filtered_music_files, args.output)
 
             for music_file in filtered_music_files:
                 print(f"Fichier: {music_file}")
@@ -111,6 +143,9 @@ def main():
                 for key, value in metadata.items():
                     print(f"{key}: {value}")
                 print("\n")
+
+            if args.output is not None:
+                create_xspf_playlist([args.file], args.output)
 
 
 if __name__ == '__main__':
